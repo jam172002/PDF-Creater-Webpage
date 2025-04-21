1430a363527fe354491f7ce1a56d8e32334c2978
jam172002 <jam172002@gmail.com>
2025-04-21 10:09:51 +0500

    Section 3 Updated and added the loading stored data logic.

--- ui/page_3.js
+++ ui/page_3.js
@@ -31,24 +31,8 @@ function Container3(doc) {
 
             let table = createTable(doc, yPosition, '', '');
             let yPos = table.getPosition();
-
-        // Add first and second rows (existing) 
-            yPos = table.addRow('11', 'Valuation Note', '', 8, true);
-            yPos = table.addRow('a (i) ', 'Mention the value as per Government Approved Rates also', Container_3.govtValue);
-
-            yPos = table.addRow('ii', 'In case of variation of 20 % or more in the valuation proposed by the valuer and the Guideline value provided in the State Govt. notification or Income Tax Gazette justification on variation has to be given',Container_3.variationJustification);
-            yPos = table.addRow('b', 'Summary of Valuation', '', 8, true);
-            yPos = table.addRow('i', 'Guideline Rate ',Container_3.guidelineRate);
-            yPos = table.addRow('ii', 'Market Rate',Container_3.marketRate);
-
-            yPos = table.addRow('iii', 'Land Value ',Container_3.landValue);
-            yPos = table.addRow('iv', 'Building Value ',Container_3.buildingValue);
-            yPos = table.addRow('v', 'Extra items, Amenities & Services',Container_3.extraItems);
-            yPost = table.addRow('vi', 'Fair Market Value (SAY)', Container_3.fairMarketValue);
-            yPost = table.addRow('c(vii)', ' Expected Rental Value ', Container_3.rentalValue);
-
             // Add data rows with all columns        
-            yPost = table.addTableHeader();   
+            yPos = table.addTableHeader();   
 
             yPos = table.addRow('', 'AS PER ACTUALS', '', 8, true);
 
@@ -78,6 +62,48 @@ function Container3(doc) {
 
                 yPos = table.addRowT(index + 1, floor, roofType, area, rate, amount, deprn, deprnAmount, netAmount);
             });
+
+             // section B: EXTRA ITEM
+             yPos = table.addRow('B', 'EXTRA ITEM', 'Amount in Rs.',6, true);
+             yPos = table.addRow('1', 'Portico', Container_3.porticoStatus);
+             yPos = table.addRow('2', 'Ornamental front door', Container_3.frontDoorStatus);
+             yPos = table.addRow('3', 'Sit out / Verandah with steel grills', Container_3.verandahStatus);
+             yPos = table.addRow('4', 'Bore well / Overhead Tank / Sump / Solar', Container_3.boreWellStatus);
+             yPos = table.addRow('5', 'Extra steel / Collapsible gates', Container_3.gatesStatus);
+             yPos = table.addRow('', 'Extra Item Totals', Container_3.extraItemsTotal);
+             
+             // section C: AMENITIES
+             yPos = table.addRow('C', 'AMENITIES', '',6, true);
+             yPos = table.addRow('1', 'Wardrobes', Container_3.wardrobesStatus);
+             yPos = table.addRow('2', 'Glazed Tiles', Container_3.glazedTilesStatus);
+             yPos = table.addRow('3', 'Extra sinks & bath tubs', Container_3.bathTubsStatus);
+             yPos = table.addRow('4', 'Marble / Ceramic tiles flooring', Container_3.marbleFlooringStatus);
+             yPos = table.addRow('5', 'Interior decorations', Container_3.interiorStatus);
+             yPos = table.addRow('6', 'Architectural Elevation works', Container_3.architecturalStatus);
+             yPos = table.addRow('7', 'Paneling works', Container_3.panelingStatus);
+             yPos = table.addRow('8', 'Aluminium works', Container_3.aluminiumStatus);
+             yPos = table.addRow('9', 'Aluminium hand rails', Container_3.handRailsStatus);
+             yPos = table.addRow('10', 'False ceilings', Container_3.falseCeilingStatus);
+             yPos = table.addRow('', ' Amenities Totals', Container_3.amenitiesTotal);
+ 
+             //section D: MISCELLANEOUS
+             yPos = table.addRow('D', 'MISCELLANEOUS', '',6, true);
+             yPos = table.addRow('1', 'Separate toilet room', Container_3.separateToilet);
+             yPos = table.addRow('2', 'Separate lumber room', Container_3.separateLumber);            
+             yPos = table.addRow('', 'Miscellaneous Totals', Container_3.miscellaneousTotal);
+ 
+             //section E: SERVICES
+             yPos = table.addRow('E', 'SERVICES', '',6, true);
+             yPos = table.addRow('1', 'Water Supply Arrangements', Container_3.waterSupplyArrangement);
+             yPos = table.addRow('2', 'Drainage Arrangements', Container_3.drainageArrangements);
+             yPos = table.addRow('3', 'Compound wall / Gate', Container_3.compoundWall);
+             yPos = table.addRow('4', 'C.B. Deposits, Fittings, Power etc.,', Container_3.depositsFittings);
+             yPos = table.addRow('5', 'Pavement', Container_3.pavement);
+             yPos = table.addRow('', 'Services Totals', Container_3.servicesTotal);
+ 
+             // Grand Total Section            
+             yPos = table.addRow('', 'Grand Totals', Container_3.grandTotal);
+
               // Resolve the promise after the table is completed
               resolve(yPos);
             };
@@ -184,8 +210,8 @@ function Container3(doc) {
             rate: 15,
             amountIn: 26,
             deprnIn: 14,
-            deprnAmount: 25,
-            netAmount: 29
+            deprnAmount: 24,
+            netAmount: 27.8
         };
 
         function addTableHeader(){
